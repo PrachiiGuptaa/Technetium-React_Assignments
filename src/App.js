0b157c13c7b997925f63cf86a30059a88281a16c
PrachiiGuptaa <guptaprachi2098@gmail.com>
2023-05-05 09:05:12 +0530

    solution15

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-// import './App.css';
 import GetOTP from './Components/GetOTP-Axios/GetOTP';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
         <GetOTP/>
       </header>
     </div>
