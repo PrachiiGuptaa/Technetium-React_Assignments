4b43b962b6946475139fcf26cd45321f2729906d
PrachiiGuptaa <guptaprachi2098@gmail.com>
2023-05-09 12:59:59 +0530

    solution16

--- src/Components/HomePage/HeroSection.js
+++ src/Components/HomePage/HeroSection.js
@@ -9,7 +9,7 @@ function HeroSection() {
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       <button className={styles.btn}>Learn More</button>
       </div>
-      <div>
+      <div className={styles.image}>
       <img src='https://img.freepik.com/free-photo/rpa-concept-with-blurry-hand-touching-screen_23-2149311914.jpg?w=740&t=st=1683578007~exp=1683578607~hmac=56265ae79347dbb24c892be39c4a3e33d66baa5ebf0bb456bed464fb89ec6cf9' alt='RandomImage'/>
       </div>
     </div>
