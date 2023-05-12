4c849ebae8ae72ef650460f27333874e476f0cd5
PrachiiGuptaa <guptaprachi2098@gmail.com>
2023-05-12 22:33:01 +0530

    solution19

---
+++ src/Components/Atoms/Task.js
@@ -0,0 +1,16 @@
+import '../Styles/Styles.css';
+
+export default function Task({ task, index, completeTask, removeTask }) {
+    return (
+        <div
+            className="task"
+            style={{ textDecoration: task.completed ? "line-through" : "" }}
+        >
+            {task.title}
+
+            <button style={{ background: "red" }} onClick={() => removeTask(index)}>x</button>
+            <button onClick={() => completeTask(index)}>Complete</button>
+
+        </div>
+    );
+}
---
+++ src/Components/Organisms/Todo.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import Task from '../Atoms/Task';
+import CreateTask from '../Molecules/CreateTask';
+import '../Styles/Styles.css';
+
+function Todo() {
+    const [tasksRemaining, setTasksRemaining] = useState(0);
+    const [tasks, setTasks] = useState([
+        {
+            title: "Complete your Assignment",
+            completed: true
+        },
+        {
+            title: "Check E-mails",
+            completed: true
+        },
+        {
+            title: "Hangout with friends",
+            completed: false
+        }
+    ]);
+
+    useEffect(() => { setTasksRemaining(tasks.filter(task => !task.completed).length) }, [tasks]);
+
+
+    const addTask = title => {
+        const newTasks = [...tasks, { title, completed: false }];
+        setTasks(newTasks);
+    };
+
+    const completeTask = index => {
+        const newTasks = [...tasks];
+        newTasks[index].completed = true;
+        setTasks(newTasks);
+    };
+
+    const removeTask = index => {
+        const newTasks = [...tasks];
+        newTasks.splice(index, 1);
+        setTasks(newTasks);
+    };
+
+    return (
+        <div className="todo-container">
+            <div className="header">Pending tasks ({tasksRemaining})</div>
+            <div className="tasks">
+                {tasks.map((task, index) => (
+                    <Task
+                    task={task}
+                    index={index}
+                    completeTask={completeTask}
+                    removeTask={removeTask}
+                    key={index}
+                    />
+                ))}
+            </div>
+            <div className="create-task" >
+                <CreateTask addTask={addTask} />
+            </div>
+        </div>
+    );
+}
+
+export default Todo;
